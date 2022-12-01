eebda2b7b7eb4f12dd82659342f25cc0d536fc5b
Владислав <vladik13918@gmail.com>
2022-12-02 00:20:01 +0200

    Create AmazonS3.cs

---
+++ lab2/AmazonS3.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2;
+
+using lab2.Interfaces;
+
+public class AmazonS3 : Singleton<AmazonS3>, IStorage
+{
+    public AmazonS3()
+    {
+        Console.WriteLine($"{nameof(AmazonS3)} ctor");
+    }
+
+    public void Write(string fileName, byte[] bytes)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public byte[] Read(string fileName)
+    {
+        throw new System.NotImplementedException();
+    }
+}
