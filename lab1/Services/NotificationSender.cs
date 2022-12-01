b54850d9029ae6140a403b5c822156c2636bbf6b
Владислав <vladik13918@gmail.com>
2022-12-02 00:19:52 +0200

    Create NotificationSender.cs

---
+++ lab1/Services/NotificationSender.cs
@@ -0,0 +1,16 @@
+﻿namespace lab1.Services;
+
+using System;
+using lab1.Interfaces;
+using lab1.Core;
+
+
+public class NotificationSender : ISender
+{
+    public NotificationTypes SenderType => NotificationTypes.Messenger;
+
+    public void SendNotification(User recipient, Notification notification)
+    {
+        Console.WriteLine("Sends messenger notification");
+    }
+}
