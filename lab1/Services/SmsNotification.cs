610232f22e13666ddf853773be6f70fe2b63a33b
Владислав <vladik13918@gmail.com>
2022-12-02 00:19:58 +0200

    Create SmsNotification.cs

---
+++ lab1/Services/SmsNotification.cs
@@ -0,0 +1,16 @@
+﻿namespace lab1.Services;
+
+using System;
+using lab1.Interfaces;
+using lab1.Core;
+
+
+public class SmsNotification : ISender
+{
+    public NotificationTypes SenderType => NotificationTypes.Sms;
+
+    public void SendNotification(User recipient, Notification notification)
+    {
+        Console.WriteLine("Sends SMS notification");
+    }
+}
