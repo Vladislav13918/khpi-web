a66bdbde2f91d84f995710c618e39331017423e7
Владислав <vladik13918@gmail.com>
2022-12-02 00:19:42 +0200

    Create ISender.cs

---
+++ lab1/Interfaces/ISender.cs
@@ -0,0 +1,10 @@
+﻿namespace lab1.Interfaces;
+
+using lab1.Core;
+
+
+public interface ISender
+{
+    public NotificationTypes SenderType { get; }
+    void SendNotification(User recipient, Notification notification);
+}
