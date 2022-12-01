2b2e1e9fb0ad0f6aedd08c24bad2b64f1090d15f
Владислав <vladik13918@gmail.com>
2022-12-02 00:20:10 +0200

    Create Singleton.cs

---
+++ lab2/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2;
+
+
+public class Singleton<T> where T : class, new()
+{
+    private static readonly Dictionary<Type, object> Instances = new ();
+
+    public static T CreateInstance()
+    {
+        if (Instances.TryGetValue(typeof(T), out object instance))
+        {
+            return (T)instance;
+        }
+
+        var newInstance = new T();
+
+        Instances.Add(typeof(T), newInstance);
+
+        return newInstance;
+    }
+}
