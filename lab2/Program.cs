de461df2537b4be3ed5576eba4ee78f92da38a84
Владислав <vladik13918@gmail.com>
2022-12-02 00:20:08 +0200

    Create Program.cs

---
+++ lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        User user1 = new User
+        {
+            FirstName = "Emmi",
+            LastName = "Adans",
+            FileStorage = AmazonS3.CreateInstance()
+        };
+
+        User user2 = new User
+        {
+            FirstName = "Taylor",
+            LastName = "Hebert",
+            FileStorage = LocalFile.CreateInstance()
+        };
+    }
+}
