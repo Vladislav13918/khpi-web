c00000c94d6b8543da7d028c54e1cc078adf56c1
Владислав <vladik13918@gmail.com>
2022-12-02 00:19:55 +0200

    Create NotificationSenderProvider.cs

---
+++ lab1/Services/NotificationSenderProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace lab1.Services;
+
+using lab1.Interfaces;
+
+
+public class NotificationSenderProvider
+{
+    public ISender DefaultSender { get; set; }
+
+    public void SetDefaultSender(NotificationTypes senderType)
+    {
+        // sets default sender.
+    }
+
+    public ISender GetSender(NotificationTypes senderType)
+    {
+        // returns sender by its sender type. if not found - returns default sender.
+        return null;
+    }
+
+    public void AddSender(ISender sender)
+    {
+        // adds available sender.
+    }
+
+    public void RemoveSender(NotificationTypes senderType)
+    {
+        // removes sender.
+    }
+}
