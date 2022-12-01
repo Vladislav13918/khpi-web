d17716aaf184863da279b5e6f36ccc11d36e0285
Владислав <vladik13918@gmail.com>
2022-12-02 00:19:49 +0200

    Create EmailNotification.cs

---
+++ lab1/Services/EmailNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace lab1.Services;
+
+using System;
+using lab1.Interfaces;
+using lab1.Core;
+
+public class EmailNotification : ISender
+{
+    public NotificationTypes SenderType => NotificationTypes.Email;
+
+    public void SendNotification(User recipient, Notification notification)
+    {
+        Console.WriteLine("Sends email notification");
+    }
+}
