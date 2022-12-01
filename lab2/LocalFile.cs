c3ab46b54172d21acb56e4117b0eb116437f5388
Владислав <vladik13918@gmail.com>
2022-12-02 00:20:07 +0200

    Create LocalFile.cs

---
+++ lab2/LocalFile.cs
@@ -0,0 +1,21 @@
+﻿namespace lab2;
+
+using lab2.Interfaces;
+
+public class LocalFile : Singleton<LocalFile>, IStorage
+{
+    public LocalFile()
+    {
+        Console.WriteLine($"{nameof(LocalFile)} ctor");
+    }
+
+    public void Write(string fileName, byte[] bytes)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public byte[] Read(string fileName)
+    {
+        throw new System.NotImplementedException();
+    }
+}
